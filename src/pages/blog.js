2d1c822bcab8ff1e5cfa2d34a08d38aab2f69eeb
rondieli <mr.rondie@gmail.com>
2019-01-03 23:14:42 -0500

    splited index and blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -37,34 +37,34 @@ const Hero = styled.div`
   }
 `;
 
-const IndexPage = ({
+const BlogPage = ({
   data: {
     allMarkdownRemark: { edges: postEdges },
   },
 }) => (
   <Layout>
     <Wrapper>
-      <Hero>
-        <h1>Stefanie Fiore</h1>
-        <p>
-          Boo
-        </p>
-        <Link to="/blog">
-          <Button big>
-            <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-            </svg>
-            Read!
-          </Button>
-        </Link>
-      </Hero>
+      <Content>
+        <SectionTitle>Latest stories</SectionTitle>
+        {postEdges.map(post => (
+          <Article
+            title={post.node.frontmatter.title}
+            date={post.node.frontmatter.date}
+            excerpt={post.node.excerpt}
+            timeToRead={post.node.timeToRead}
+            slug={post.node.fields.slug}
+            category={post.node.frontmatter.category}
+            key={post.node.fields.slug}
+          />
+        ))}
+      </Content>
     </Wrapper>
   </Layout>
 );
 
-export default IndexPage;
+export default BlogPage;
 
-IndexPage.propTypes = {
+BlogPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array.isRequired,
@@ -72,8 +72,8 @@ IndexPage.propTypes = {
   }).isRequired,
 };
 
-export const IndexQuery = graphql`
-  query IndexQuery {
+export const BlogQuery = graphql`
+  query BlogQuery {
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
